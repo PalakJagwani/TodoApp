cbf64ff698d73653ce641ea1688bf54f2529c400
Palak Jagwani <jagwanipalak02@gmail.com>
2024-06-10 17:23:09 +0530

    update todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Todos from "./components/Todos"
 function App() {
   return (
     <div className=' text-slate-100 grid place-content-center mt-24'>
-      <h1 className=" text-3xl font-serif font-semibold text-center">Add your todos here..</h1>
+      <h1 className=" text-3xl font-serif font-semibold text-center">Add your todos here...</h1>
       <InputForm/>
       <Todos/>
     </div>
